991cca20e8432605aa69f5d9355ff2ad93289e73
Eray Ceviker <erayceviker0@gmail.com>
2022-09-22 15:59:18 +0300

    Create Layers, Add Migration

---
+++ src/projects/kodalama.io.Devs/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration;
+
+        DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProgrammingLanguage>(p =>
+            {
+                p.ToTable("ProgrammingLanguages").HasKey(k => k.Id);
+                p.Property(p => p.Id).HasColumnName("Id");
+                p.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            ProgrammingLanguage[] programmingLanguageSeeds = { new(1, "Python"), new(2, "CSharp"), new(3, "Java") };
+
+            modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageSeeds);
+        }
+    }
+}
